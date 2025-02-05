f43a4388cbe1ac9f8ecf2926b9f6f9efd359769c
Nohit Singh Chouhan <nohitsinghchouhan594@gmail.com>
2025-02-05 20:10:34 +0530

    api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const API_KEY = "PUT YOUR API KEY HERE";      // GO TO WEBSITE themoviedb.org  
-const BASE_URL = "https://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";  //NOTE : use vpn to connect also you can install VeePN chrome Extension
 
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
